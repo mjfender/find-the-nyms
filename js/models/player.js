533fba2d7cde54ca941c709a5a1ca9cca7a5a5a6
Matt Fender <mjfender@gmail.com>
2017-01-27 15:02:30 -0500

    added color changes and round ending logic

--- js/models/player.js
+++ js/models/player.js
@@ -2,6 +2,7 @@ class Player {
   constructor(id) {
     this.id = id
     this.score = 0
+    this.roundScore = 0
     this.mistakes = 0
     // this.correct_words = []
   }
@@ -9,11 +10,13 @@ class Player {
   makesMistake() {
     this.mistakes ++
     this.score --
+    this.roundScore --
   }
 
   winsPoint(word) {
     // this.correct_words.push(word)
     this.score ++
+    this.roundScore ++
   }
 
 
