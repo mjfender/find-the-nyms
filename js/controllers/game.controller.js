d85f3032ba58154e9299324355d4a3fcb7da5f7d
Matt Fender <mjfender@gmail.com>
2017-01-27 17:34:06 -0500

    completed END ROUND logic

--- js/controllers/game.controller.js
+++ js/controllers/game.controller.js
@@ -3,17 +3,18 @@ class GameController {
   constructor() {
     this.$rootWord = $('#rootWord')
     this.$scoreboard = $('#scoreboard')
+    this.$roundboard = $('#roundboard')
+
     this.$wordList = $('#wordList')
     this.$startButton = $('#start-button')
-    this.$roundboard = $('#roundboard')
+    this.$startText = $('#start-text')
   }
 
   init() {
     this.$rootWord.hide()
     this.$scoreboard.hide()
-    this.$wordList.hide()
     this.$roundboard.hide()
-
+    this.$wordList.hide()
 
     this.$startButton.click( (event) => {
       this.startGame()
@@ -26,8 +27,9 @@ class GameController {
     this.buildRandomWord()
     this.$rootWord.show()
     this.$scoreboard.show()
-    this.$wordList.show()
     this.$roundboard.show()
+
+    this.$wordList.show()
     this.startRound(newGame)
     // endRound()
     // startRound()
@@ -37,6 +39,26 @@ class GameController {
     $allWordListEls.click((event) => this.eventHandler(event, newGame))
 
 
+  }
+
+  nextRound(newGame) {
+    this.$rootWord.hide()
+    this.$scoreboard.hide()
+    this.$wordList.hide()
+    this.$roundboard.hide()
+    this.$startText.html('Start Next Round')
+    this.$startButton.show()
+
+    this.$startButton.click( (event) => {
+      this.startRound(newGame)
+    })
+
+  }
+
+  startRound(newGame){
+    this.$rootWord.show()
+    this.$wordList.show()
+    this.round ++
   }
 
   buildRandomWord(){
@@ -50,42 +72,45 @@ class GameController {
     this.$wordList.hide()
   }
 
-
-  startRound(newGame){
-    this.$rootWord.show()
-    this.$wordList.show()
-    this.round ++
-
-
-  }
-
   eventHandler(event, newGame){
     var wordInfo = event.currentTarget.id
-    var [wordName, wordType] = wordInfo.split("_")
-    var value = newGame.getLevelValue()
-    var currentPlayer = newGame.currentPlayer()
-    var $wordDivInDOM = $(`#${wordInfo}`)
+    var wordType = wordInfo.split("_")[1]
+    this.interpretMove(newGame, wordInfo, wordType)
+    // pass the ball
+  }
 
 
-      if (value.includes(wordType)) {
-        $wordDivInDOM.css('background-color', '#A9FFCB')
-        currentPlayer.winsPoint()
-      } else {
-        $wordDivInDOM.css('background-color', '#EF271B')
-        currentPlayer.makesMistake()
-      }
-      $('#player').html(`Player ${currentPlayer.id}`)
-      $('#score').html(`Score: ${currentPlayer.score}`)
+  interpretMove(newGame, wordInfo, wordType){ 
+  var value = newGame.getLevelValue()
+  var currentPlayer = newGame.currentPlayer()
+  var $wordDivInDOM = $(`#${wordInfo}`)
 
-      if (currentPlayer.roundScore >= 5 || currentPlayer.mistakes >= 3) {
-        alert('Round over, boo')
-      }
-
-      $wordDivInDOM.unbind("click")
-      console.log("It worked!")
+    if (value.includes(wordType)) {
+      $wordDivInDOM.css('background-color', '#A9FFCB')
+      currentPlayer.winsPoint()
+    } else {
+      $wordDivInDOM.css('background-color', '#EF271B')
+      currentPlayer.makesMistake()
     }
-  // define the listener function that checks whether right or not
-  // and make all the changes necessary to score, mistakes, etc
+    
+    $('#player').html(`Player ${currentPlayer.id}`)
+    $('#score').html(`Score: ${currentPlayer.score}`)
+    $('#mistakes').html(`Mistakes: ${currentPlayer.mistakes}`)
+    $wordDivInDOM.unbind("click")
+
+    if (this.roundOver(newGame)) {
+      this.nextRound(newGame)
+    }
+  }
+
+
+  roundOver(newGame) {
+    return newGame.currentPlayer().roundScore >= 5 || newGame.currentPlayer().mistakes >= 3
+    // ADD A TIMER WHILE LOOP???
+  }
 
 
 }
+  // define the listener function that checks whether right or not
+  // and make all the changes necessary to score, mistakes, etc
+
