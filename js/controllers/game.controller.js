8fe065744915b6061133ad6df91d59921366df1a
Matt Fender <mjfender@gmail.com>
2017-01-28 17:26:32 -0500

    fixed bug. added root words. added messageboard that updates with instructions. added logic to advance levels. created system for not reusing seed words. works through level 2.

--- js/controllers/game.controller.js
+++ js/controllers/game.controller.js
@@ -7,6 +7,7 @@ class GameController {
     this.$wordList = $('#wordList')
     this.$startButton = $('#start-button')
     this.$startText = $('#start-text')
+    this.$messageboard = $('#messageboard')
   }
 
   init() {
@@ -14,46 +15,31 @@ class GameController {
     this.$scoreboard.hide()
     this.$roundboard.hide()
     this.$wordList.hide()
-
+    this.$messageboard.html("<h1>Welcome</h1><h4>Grab a friend and get ready to play!</h4> <p>Learn about synonyms and antonyms while you discover new words.<br> Earn up to 5 points each round. <br> The round ends when you make three mistakes. <br><strong>The first player to 20 points wins!</strong></h1>")
     this.$startButton.click( (event) => {
       this.startGame()
     })
   }
 
   startGame() {
-    this.$startButton.hide()
     var newGame = new Game()
-    this.startRound(newGame)
+    this.nextRound(newGame)
   }
 
-  nextRound(newGame) {
-    this.$rootWord.hide()
-    this.$scoreboard.hide()
-    this.$wordList.hide()
-    this.$roundboard.hide()
-    this.$startText.html('Start Next Round')
-    this.$startButton.show()
-    $("#rootWord_h3").remove()
-    $(".js--gameWords").remove()
-    newGame.round ++
-    var currentPlayer = newGame.currentPlayer()
-    currentPlayer.roundScore = 0
-    currentPlayer.mistakes = 0
-
-    this.$startButton.click( (event) => {
-      this.startRound(newGame)
-    })
-
-  }
 
   startRound(newGame){
     this.$startButton.hide()
+    this.$startButton.unbind("click")
+    this.$messageboard.html(newGame.currentRulesShort())
+    this.$messageboard.show()
+
     this.buildRandomWord()
     this.$rootWord.show()
     this.$wordList.show()
     $('#player').html(`Player ${newGame.currentPlayer().id}`)
     $('#score').html(`Score: ${newGame.currentPlayer().score}`)
     $('#mistakes').html(`Mistakes: ${newGame.currentPlayer().mistakes}`)
+    $('#level').html(`Level: ${newGame.level}`)
     this.$scoreboard.show()
     this.$roundboard.show()
 
@@ -61,15 +47,38 @@ class GameController {
     $allWordListEls.click((event) => this.eventHandler(event, newGame))
   }
 
+  nextRound(newGame) {
+    if (newGame.firstRun) {
+      newGame.firstRun = false
+    } else {
+      newGame.advanceRound()
+    }
+
+    var currentPlayer = newGame.currentPlayer()
+    currentPlayer.roundScore = 0
+    currentPlayer.mistakes = 0
+
+    this.$rootWord.empty()
+    this.$scoreboard.hide()
+    this.$wordList.hide()
+    this.$roundboard.hide()
+    this.$messageboard.html(newGame.currentRules())
+    this.$messageboard.show()
+    this.$startText.html(`Player ${currentPlayer.id}: Start Next Round`)
+    this.$startButton.show()
+    $(".js--gameWords").remove()
+    
+
+    this.$startButton.click( (event) => {
+      this.startRound(newGame)
+    })
+
+  }
+
   buildRandomWord(){
     var newWord = Word.getRandom()
     newWord.build()
   }
-
-  // endRound(){
-  //   this.$rootWord.hide()
-  //   this.$wordList.hide()
-  // }
 
   eventHandler(event, newGame){
     var wordInfo = event.currentTarget.id
